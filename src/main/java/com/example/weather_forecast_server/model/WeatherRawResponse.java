2b862d8e0d0c6bcbfb8ba41ae0d1e28c32c97c10
Nguyen Minh Tuan Khanh <khanhblue06@gmail.com>
2025-08-12 21:22:33 +0700

    fix rawrespone

--- src/main/java/com/example/weather_forecast_server/model/WeatherRawResponse.java
+++ src/main/java/com/example/weather_forecast_server/model/WeatherRawResponse.java
@@ -11,33 +11,33 @@ public class WeatherRawResponse {
     private String timezone;
     private String address;
     private List<WeatherDay> days;
-
-    @Data
-    public static class WeatherDay {
-        private String datetime;
-        private double temp;
-        private double tempmax;
-        private double tempmin;
-        private String icon;
-        private double humidity;
-        private double windspeed;
-        private double pressure;
-        private String sunrise;
-        private String sunset;
-        private int uvindex;
-        private List<WeatherHour> hours;
-    }
-
-    @Data
-    public static class WeatherHour {
-        private String datetime;
-        private double temp;
-        private double tempmax;
-        private double tempmin;
-        private String icon;
-        private double humidity;
-        private double windspeed;
-        private double pressure;
-        private int uvindex;
-    }
 }
+@Data
+ class WeatherDay {
+    private String datetime;
+    private double temp;
+    private double tempmax;
+    private double tempmin;
+    private String icon;
+    private double humidity;
+    private double windspeed;
+    private double pressure;
+    private String sunrise;
+    private String sunset;
+    private int uvindex;
+    private List<WeatherHour> hours;
+}
+
+@Data
+class WeatherHour {
+    private String datetime;
+    private double temp;
+    private double tempmax;
+    private double tempmin;
+    private String icon;
+    private double humidity;
+    private double windspeed;
+    private double pressure;
+    private int uvindex;
+}
+
